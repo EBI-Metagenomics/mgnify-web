71ef1a6b9746e967de2d3a0cfba0f360b437fa83
Mahfouz Shehu <mahfouz@mh7346320n.windows.ebi.ac.uk>
2023-08-04 18:02:32 +0100

    WIP, motus crate preparation and html files navigation functionality achieved

--- scripts/prepare_motus_crates/prepare_motus_crates.py
+++ scripts/prepare_motus_crates/prepare_motus_crates.py
@@ -1,10 +1,11 @@
+import argparse
+import glob
+import json
+import logging
 import os
 import requests
-import tarfile
 import shutil
-import glob
-import logging
-import argparse
+import tarfile
 
 
 class MotusCratePreparer:
@@ -16,6 +17,27 @@ class MotusCratePreparer:
         self.multiqc_path = None
         self.krona_files = None
         self.output_folder_name = None
+
+    def prepare_motus_crate(self):
+        logging.info("Starting the script.")
+
+        try:
+            self.setup_logging()
+            self.create_temp_dir()
+            self.get_srr_value()
+            zip_file_path = self.download_zip_file()
+            self.extract_zip_file(zip_file_path)
+            self.find_multiqc_report()
+            self.find_krona_files()
+            self.create_new_folder()
+            self.copy_files_to_new_folder()
+            self.create_preview_html()
+            self.create_ro_crate_metadata()  # Add the creation of ro-crate-metadata.json
+            self.zip_new_folder()
+            self.clean_up()
+        except Exception as e:
+            logging.error(str(e))
+            raise
 
     def setup_logging(self):
         logging.basicConfig(
@@ -73,53 +95,74 @@ class MotusCratePreparer:
 
     def create_preview_html(self):
         srr_folder_path = os.path.join(self.temp_dir, self.srr_value)  # Use the specific SRR folder
-        preview_content = f"""
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <title>Preview</title>
-        </head>
-        <body>
-            <h1>Preview of Extracted HTML Files</h1>
-            <ul>
-                <li><a href="multiqc_report.html" id="multiqc_report.html">multiqc_report.html</a></li>
-                {"".join(f'<li><a href="krona_{subfolder_name}.html" id="krona_{subfolder_name}.html">krona_{subfolder_name}.html</a></li>'
-                          for subfolder_name in os.listdir(os.path.join(srr_folder_path, 'taxonomy'))
-                          if not subfolder_name.__contains__('DS_Store') and not subfolder_name.startswith('._'))}
-            </ul>
+
+        # Add the JavaScript script for the event listener
+        js_script = """
+        <script>
+          function containsText(anchor, text) {
+            return anchor.textContent.includes(text);
+          }
+        
+          function handleClick(event) {
+            if (window.self === window.top) {
+              // We're not in an iframe, so we allow the links to work normally
+              return;
+            }
+            event.preventDefault();
+            const anchor = event.target;
+            const anchorText = anchor.textContent;
+            window.parent.postMessage(anchor.id, "*");
+          }
+        
+          document.addEventListener("DOMContentLoaded", function() {
+            const anchorTags = document.querySelectorAll("a");
+            anchorTags.forEach(anchor => {
+              const anchorText = anchor.textContent;
+              if (containsText(anchor, "multiqc_report") || containsText(anchor, "krona")) {
+                anchor.addEventListener("click", handleClick);
+              }
+            });
+          });
+        </script>
+        """
+
+        # Update the preview_content to include the unique IDs and the JavaScript script
+        preview_content = f""" <!DOCTYPE html> <html> <head> <title>Preview</title> </head> <body> <h1>Preview of 
+        Extracted HTML Files</h1> <ul> <li><a href="multiqc_report.html" 
+        id="multiqc_report.html">multiqc_report.html</a></li> 
+        {"".join(f'<li><a href="krona_{subfolder_name}.html" id="krona_{subfolder_name}'
+                 f'.html">krona_{subfolder_name}.html</a></li>'
+                 for subfolder_name in os.listdir(os.path.join(srr_folder_path, 'taxonomy'))
+                 if not subfolder_name.__contains__('DS_Store') and not subfolder_name.startswith('._'))}
+        </ul>
+        {js_script}
         </body>
         </html>
         """
 
-        with open(os.path.join(self.output_folder_name, 'preview.html'), 'w') as f:
+        with open(os.path.join(self.output_folder_name, 'ro-crate-preview.html'), 'w') as f:
             f.write(preview_content)
+
+    def create_ro_crate_metadata(self):
+        # Replace the placeholders below with the actual metadata content
+        metadata = {
+            "title": "My RO-Crate",
+            "description": "This is a RO-Crate metadata file",
+            # Add more metadata fields as needed
+        }
+
+        # Write the metadata to the ro-crate-metadata.json file
+        ro_crate_metadata_path = os.path.join(self.output_folder_name, 'ro-crate-metadata.json')
+        with open(ro_crate_metadata_path, 'w') as f:
+            json.dump(metadata, f, indent=2)
 
     def zip_new_folder(self):
         shutil.make_archive(self.output_folder_name, 'zip', self.output_folder_name)
-        shutil.move(f"{self.output_folder_name}.zip", os.path.join(self.destination_folder, f"motus_{self.srr_value}.zip"))
+        shutil.move(f"{self.output_folder_name}.zip",
+                    os.path.join(self.destination_folder, f"motus_{self.srr_value}.zip"))
 
     def clean_up(self):
         shutil.rmtree(self.temp_dir)
-
-    def prepare_motus_crate(self):
-        logging.info("Starting the script.")
-
-        try:
-            self.setup_logging()
-            self.create_temp_dir()
-            self.get_srr_value()
-            zip_file_path = self.download_zip_file()
-            self.extract_zip_file(zip_file_path)
-            self.find_multiqc_report()
-            self.find_krona_files()
-            self.create_new_folder()
-            self.copy_files_to_new_folder()
-            self.create_preview_html()
-            self.zip_new_folder()
-            self.clean_up()
-        except Exception as e:
-            logging.error(str(e))
-            raise
 
 
 if __name__ == "__main__":
